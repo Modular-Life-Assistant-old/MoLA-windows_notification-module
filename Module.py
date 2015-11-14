e61ff5d1e0ff7dda45fb785ea9aa670ff45a7b77
Treemo <treemo@hotmail.fr>
2015-11-14 18:48:41 +0100

    add image ico

--- Module.py
+++ Module.py
@@ -1,3 +1,4 @@
+import tempfile
 from core import settings
 from helpers.modules.NotificationModule import NotificationModule
 
@@ -15,23 +16,16 @@ except ImportError:
 
 class Module(NotificationModule):
     def init(self):
-        def on_destroy(self, hwnd, msg, wparam, lparam):
-            Shell_NotifyIcon(NIM_DELETE, (hwnd, 0))
-            PostQuitMessage(0)
-
         # Register the Window class.
         wc = WNDCLASS()
         wc.hInstance = GetModuleHandle(None)
         wc.lpszClassName = "PythonTaskbar"
-        wc.lpfnWndProc = {
-                win32con.WM_DESTROY: on_destroy,
-        }
         self.classAtom = RegisterClass(wc)
 
     def is_available(self):
         return os.name == 'nt'
 
-    def send(self, msg):
+    def send(self, msg, image=None, sound=None):
         # Create the Window.
         style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
         hwnd = CreateWindow(
@@ -40,17 +34,20 @@ class Module(NotificationModule):
             0, 0, GetModuleHandle(None), None
         )
         UpdateWindow(hwnd)
-        hicon = LoadIcon(0, win32con.IDI_APPLICATION)
 
-        """
-        iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
-        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
-        hicon = LoadImage(hinst, iconPathName, win32con.IMAGE_ICON, 0, 0, icon_flags)
-        """
+        if image:
+            img_path = os.path.join(tempfile.gettempdir(), 'mola_notif.ico')
+            image.save(img_path)
+            flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+            hicon = LoadImage(0, img_path, win32con.IMAGE_ICON, 0, 0, flags)
+            # TODO: add thumbnail in notification
+            # TODO: action click on notification (open image)
+
+        else:
+            hicon = LoadIcon(0, win32con.IDI_APPLICATION)
 
         Shell_NotifyIcon(NIM_ADD, (
-            hwnd, 0, NIF_ICON | NIF_MESSAGE | NIF_TIP, win32con.WM_USER+20, hicon, "tooltip"
-        ))
-        Shell_NotifyIcon(NIM_MODIFY, (
-            hwnd, 0, NIF_INFO, win32con.WM_USER+20, hicon, "Balloon tooltip", msg, 200, settings.NAME
+            hwnd, 0, NIF_INFO | NIF_ICON | NIF_MESSAGE | NIF_TIP,
+            win32con.WM_USER+20, hicon, "tooltip", msg, 200, settings.NAME,
+            0x20 | 0x04  # NIIF_LARGE_ICON | NIIF_USER
         ))
